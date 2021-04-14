02cf0c0b74e1149604ef999efa2ad4ab651030a6
Mathilda Pettersson <mathildapetterssonn@hotmail.com>
2021-04-14 17:17:30 +0200

    task solved

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const userName = document.getElementById("userName");
 const userMail = document.getElementById("userMail");
 const userTel = document.getElementById("userTel");
 const sendBtn = document.getElementById("sendBtn");
+const randomBtn = document.getElementById("randomBtn");
+
+printUsers();
 
 sendBtn.addEventListener("click", () => {
     sendUser();
 });
 
-printUsers();
+randomBtn.addEventListener("click", () => {
+    randomUser();
+});
+
 
 function sendUser() {
     newUser = {userName: userName.value, userMail: userMail.value, userTel: userTel.value};
@@ -34,4 +40,22 @@ function printUsers() {
             ${data[user].userTel}<br></h4></div>`);
         };
     });
+};
+
+function randomUser() {
+    console.log("random klick");
+
+    fetch('https://randomuser.me/api/')
+    .then(resp => resp.json())
+    .then(data => {
+
+        let randomUser = {userName: data.results[0].name.first + " " + data.results[0].name.last, userMail: data.results[0].email, userTel: data.results[0].phone};
+
+        console.log(randomUser);
+
+        userName.value = data.results[0].name.first;
+        userMail.value = data.results[0].email;
+        userTel.value = data.results[0].phone;
+    });
+
 };
